2235131ee87c0adfa261999a17d9d081e87b8e0c
GiulianoMacarrone <140300743+GiulianoMacarrone@users.noreply.github.com>
2025-06-28 12:00:08 -0300

    Create Rol.cs

---
+++ BE/Modelo/Rol.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Abstraccion;
+
+namespace BE.Modelo
+{
+    public class Rol
+    {
+        public int id { get; set;}
+        public string designacion { get; set; } 
+        public string permiso {get; set;}
+      
+        public List<string> ListaPermisos() => new List<string;
+
+      
+
+    }
+}
