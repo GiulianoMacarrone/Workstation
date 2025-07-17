c564ee3f65fa0a021f4a4a0ef4c82250bbeee60c
Giuliano Macarrone <gulianosebas@hotmail.com>
2025-07-17 18:11:29 -0300

    Backup

---
+++ BE/Backup/Backup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BE.Backup
+{
+    public class Backup
+    {
+        public int Id { get; set; }
+        public DateTime Fecha { get; set; }
+        public string Operacion { get; set; }  // backup o restaurar
+        public string ArchivoPath { get; set; }   
+
+    }
+}
